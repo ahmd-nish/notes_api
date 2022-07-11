3191d1b4995e6e68f8e533c53e64d883ab665065
nish-ahmd-it <ahamedmnn.19@uom.lk>
2022-07-11 12:46:45 +0530

    Made some changes in intiating the seeder file

--- AdminSeed.js
+++ AdminSeed.js
@@ -5,8 +5,8 @@ const bcrypt = require('bcrypt');
 
 const seedUser =[
    new User ({
-        firstName: 'Admin',
-        lastName: 'Seeder',
+        firstName: 'Seeder',
+        lastName: 'Admin',
         email: 'admin@seed.com',
         status: true,
         accountType: 'admin',
@@ -14,9 +14,7 @@ const seedUser =[
     })
 ]
 
-
-async function seedDB(){
-
+async function connect(){
     mongoose.connect('mongodb+srv://nishad:basin123@surge.daapw.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true, 
     useUnifiedTopology: true}).then(()=>{
@@ -25,11 +23,22 @@ async function seedDB(){
         console.log(error);
         console.log('Error connecting to MongoDB');
     })
+}
+
+async function seedDB(){
+
     
+        connect();
         let password = 'admin';
         const salt = await  bcrypt.genSalt(Number(process.env.SALT));
         const hashedPassword = await  bcrypt.hash(password, salt);
         seedUser[0].password = hashedPassword;
+        try {
+            await User.findOneAndDelete({'email': 'admin@seed.com'});
+            console.log('Initial seedUser deleted successfully');
+        } catch (error) {
+            console.log(error);
+        }
         await seedUser[0].save();
         console.log('Seeded DB');
 
